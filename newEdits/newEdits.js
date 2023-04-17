efc193bad04cfa18d044099b9e78080cbeeca887
Janis <janis.paberzs18@gmail.com>
2023-04-17 18:01:32 +0300

    edit js

--- newEdits/newEdits.js
+++ newEdits/newEdits.js
@@ -1,10 +1,21 @@
-let dropdownBtns = document.querySelectorAll('.dropdown-btn');
+"use strict";
 
-if(dropdownBtns.length > 0) {
-  for (let dropdownBtn of dropdownBtns) {
-    dropdownBtn.addEventListener('click', function () {
-      dropdownBtn.classList.toggle('active');
-      dropdownBtn.nextElementSibling.classList.toggle('active');
-    });
-  }
+
+function eventHandler() { 
+  let dropdownBtns = document.querySelectorAll('.dropdown-btn');
+  
+  if(dropdownBtns.length > 0) {
+    for (let dropdownBtn of dropdownBtns) {
+      dropdownBtn.addEventListener('click', function () {
+        dropdownBtn.classList.toggle('active');
+        dropdownBtn.nextElementSibling.classList.toggle('active');
+      });
+    }
+  };
+};
+
+if (document.readyState !== 'loading') {
+	eventHandler();
+} else {
+	document.addEventListener('DOMContentLoaded', eventHandler);
 }
